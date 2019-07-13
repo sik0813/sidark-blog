fd6d94390a4880d464816226f8af89dc7cf8ceec
Youngsik Hwang <newlcb0813@naver.com>
2019-07-13 16:53:52 +0900

    일단 중간 저장

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from './Home';
-export { default as History } from './History';
+export { default as License } from './License';
 export { default as About } from './About';
 export { default as Post } from './Post';
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import 'css/bootstrap.min.css';
 import { Route } from 'react-router-dom';
-import { Home, History, About, Post } from 'pages';
+import { Home, License, About, Post } from 'pages';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Route exact path="/" component={ Home } />
-        <Route exact path="/History" component={ History }/>
+        <Route exact path="/License" component={ License }/>
         <Route exact path="/About"component={ About } />
         <Route exact path="/Post" component={ Post } />
       </div>
